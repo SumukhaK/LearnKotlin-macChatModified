06061b1cdc3152f984e47f3f7612f6318f601978
sumukha <sumukh.aithal@gmail.com>
2021-02-06 12:02:47 +0530

    making a better bew

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": "mongodb+srv://testUser:<"HTh5MyH3A10RSznM">@mongodbtest.yxsef.mongodb.net/<database>?retryWrites=true&w=majority",//"mongodb+srv://testUser:<FWEMXfr2eh4wdgBA>@mongodbtest.yxsef.mongodb.net/<test-db>?retryWrites=true&w=majority"
+  "mongoUrl": "mongodb+srv://testUser:<'HTh5MyH3A10RSznM'>@mongodbtest.yxsef.mongodb.net/<database>?retryWrites=true&w=majority",//"mongodb+srv://testUser:<FWEMXfr2eh4wdgBA>@mongodbtest.yxsef.mongodb.net/<test-db>?retryWrites=true&w=majority"
   "bodyLimit": "100kb"
 }
